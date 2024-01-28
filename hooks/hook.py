161da03c4d4a6a5e0dbe8d3a13c66ba2745d0460
Namek6969 <74002995+Namek6969@users.noreply.github.com>
2024-01-28 16:40:54 +0530

    Update hook.py

--- hooks/hook.py
+++ hooks/hook.py
@@ -31,12 +31,13 @@ except Exception as ee:
 def new():
     link = 'https://animepahe.com/api?m=airing&page=1'
     webrowse= uc.Chrome(headless=True,use_subprocess=False)
-    response=webrowse.get(link).json()
+    response=webrowse.get(link)
     webrowse.quit()
     #response = requests.get(link,headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"}).json()
     with open(errlogs,"w") as ff:
       ff.write(response)
       ff.writr("\nHello")
+    response=response.json()
     allshowsreleased=[
         [
             '{}/{}'.format(x['anime_session'], x['session']),
